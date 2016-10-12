107e2dec9d309cb3e7e900892250540425af8dec
Abhinav Dagur <abhinav.dagur@gmail.com>
2016-10-12 13:15:15 -0700

    Made changes

--- index.js
+++ index.js
@@ -27,18 +27,34 @@ restService.post('/hook', function (req, res) {
         if (req.body) {
             console.log('Request Body: ', req.body);
             var requestBody = req.body;
-
+						console.log('requestBody.result: ', requestBody.result);
             if (requestBody.result) {
                 speech = '';
-
+								console.log('fulfillment: ', requestBody.result.fulfillment);
                 if (requestBody.result.fulfillment) {
                     speech += requestBody.result.fulfillment.speech;
                     speech += ' ';
                 }
-
+								console.log('requestBody.result: ', requestBody.result);
+								console.log('requestBody.result.result.parameters.order_number: ', requestBody.result.parameters.order_number);
                 if (requestBody.result.action) {
                 	  //calling EBS WS
-                	  callGetAccessToken();
+                	 if (requestBody.result.action === 'repeat.order'){
+                	  	 processRepeatOrder(requestBody.result.parameters.order_number,function(returnedJson){
+                	  	 		console.log('result: ', returnedJson);
+                	  	 		return res.json({returnedJson});
+                	  	 });	
+                	  	 
+                	 }else if (requestBody.result.action === 'create.order'){
+                	 		processCreateOrder(requestBody.result.parameters.item_name,requestBody.result.parameters.quantity,function(returnedJson){
+                	 		console.log('result: ', returnedJson);
+                	  	 		return res.json({returnedJson});
+                	  	 });
+                	 }else if (requestBody.result.action === 'cancel.order'){
+                	 }else if (requestBody.result.action === 'query.order'){
+                	 }else if (requestBody.result.action === 'expedite.order'){
+                	 
+                	 }
                 	  
                     speech += 'EBS action: ' + requestBody.result.action;
                 }
@@ -47,11 +63,11 @@ restService.post('/hook', function (req, res) {
 
         console.log('result: ', speech);
 
-        return res.json({
+        /*return res.json({
             speech: speech,
             displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
+            source: 'ebs-team-integration'
+        });*/
     } catch (err) {
         console.error("Can't process request", err);
 
@@ -68,7 +84,55 @@ restService.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
 });
 
-function callGetAccessToken() {
+function processLastOrders(callback) {
+		getAccessToken(function(tokenName,tokenValue){
+		   
+		    	callQueryLastOrders(tokenName,tokenValue,function(inputXml){
+		    		
+		    		console.log("inputXml :"+inputXml);
+            var parser = new xml2js.Parser();
+            
+            parser.parseString(inputXml, function (err, result) {
+            	
+            	return callback(JSON.stringify(result));
+            	
+            	
+						});		    		
+		    		
+		    		
+		    	});
+	
+	})
+}
+
+
+function processRepeatOrder(orderNumber,callback) {
+		getAccessToken(function(tokenName,tokenValue){
+		   
+		    	callRepeatOrders(orderNumber,tokenName,tokenValue,function(inputXml){
+		    		
+		    		console.log("inputXml :"+inputXml);
+            var parser = new xml2js.Parser();
+            
+            parser.parseString(inputXml, function (err, result) {
+            	
+            	return callback(JSON.stringify(result));
+            	
+            	
+						});		    		
+		    		
+		    		
+		    	});
+	
+	})
+}
+
+function processCreateOrder(itemName,qty) {
+	
+	
+}
+
+function getAccessToken(callBackAccessToken) {
 
     var optionsgetAccessToken = {
         host: 'rws3220164.us.oracle.com', // here only the domain name (no http/https !)
@@ -91,7 +155,6 @@ function callGetAccessToken() {
             console.info('GET result:\n');
             console.info(d.toString());
             
-            var extractedData = "";
             var parser = new xml2js.Parser();
             
             parser.parseString(d, function (err, result) {
@@ -99,6 +162,7 @@ function callGetAccessToken() {
               tokenValue = result.response.data[0].accessToken.toString();
               console.dir(tokenName);
               console.dir(tokenValue);
+              console.info('Got Token Name and value');
 						});
             
             console.info('\n\nCall completed');
@@ -106,10 +170,8 @@ function callGetAccessToken() {
         });
         res.on('end', () => {
     			console.log('No more data in response.');
-    			console.info('tokenName :'+tokenName);
-    			console.info('tokenValue :'+tokenValue);
 	        if (res.statusCode == '200') {
-    				callPost();
+    				return callBackAccessToken(tokenName,tokenValue);
     			}
     				
   			});
@@ -123,37 +185,49 @@ function callGetAccessToken() {
     });
 }
 
-function callPost() {
-    var body = '<params>121212</params>';
-    
-    var postheaders = {
+function getOptionsPost(body,EBSFunctionName){
+	
+	var postheaders = {
         'content-type': 'text/xml',
         'Cookie': tokenName+'='+tokenValue,
         'Cache-Control': 'no-cache',
-        'Content-Length': Buffer.byteLength(body, 'utf8')
-    };
+      'Content-Length': Buffer.byteLength(body, 'utf8')}
 
-
-    var optionspost = {
-        host: 'rws3220164.us.oracle.com', // here only the domain name (no http/https !)
+	 var optionspost = {
+        host: 'rws3220164.us.oracle.com',
         port: 8003,
-        path: '/OA_HTML/RF.jsp?function_id=ONT_REST_SALES_ORDERS&resp_id=21623&resp_appl_id=660&security_group_id=0',
+        path: '/OA_HTML/RF.jsp?function_id='+EBSFunctionName+'&resp_id=21623&resp_appl_id=660&security_group_id=0',
         method: 'POST',
         headers: postheaders
     };
+ 
+ return optionspost;
+	
+	     
+}
 
-
+function callRepeatOrders(orderNumber,tokenName,tokenValue,callBackLastOrders) {
+    var body = '<params>'+orderNumber+'</params>';
+    var returnxml;
     
-    var reqPost = http.request(optionspost, function(res) {
+    var reqPost = http.request(getOptionsPost(body,'ONT_REST_SALES_ORDERS'), function(res) {
 
         //console.log("POST headers: ", res.headers);
         console.log(" POST statusCode: ", res.statusCode);
 
         res.on('data', function(d) {
             console.info('POST result:\n');
-            process.stdout.write(d);
-            console.info('\n\nPOST completed');
+            //process.stdout.write(d);
+            returnxml = d;
+            //console.info('\n\nPOST completed');
         });
+        
+        res.on('end', () => {
+	        if (res.statusCode == '200') {
+    				return callBackLastOrders(returnxml);
+    			}
+    				
+  			});        
     });
     console.info('after POST body' + body);
     // write the xml data
@@ -163,3 +237,39 @@ function callPost() {
         console.error(e);
     });
 }
+function callQueryLastOrders(tokenName,tokenValue,callBackLastOrders) {
+    var body = '<params>121212</params>';
+    var returnxml;
+    
+    var reqPost = http.request(getOptionsPost(body,'ONT_REST_SALES_ORDERS'), function(res) {
+
+        //console.log("POST headers: ", res.headers);
+        console.log(" POST statusCode: ", res.statusCode);
+
+        res.on('data', function(d) {
+            console.info('POST result:\n');
+            //process.stdout.write(d);
+            returnxml = d;
+            //console.info('\n\nPOST completed');
+        });
+        
+        res.on('end', () => {
+	        if (res.statusCode == '200') {
+    				return callBackLastOrders(returnxml);
+    			}
+    				
+  			});        
+    });
+    console.info('after POST body' + body);
+    // write the xml data
+    reqPost.write(body);
+    reqPost.end();
+    reqPost.on('error', function(e) {
+        console.error(e);
+    });
+}
+
+
+
+
+                
